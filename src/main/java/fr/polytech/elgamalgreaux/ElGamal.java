08c55ab94250fe3e77ac9062fb6f77245f1c8788
Thomas Greaux <thomas.greaux@etu.unice.fr>
2019-02-02 11:32:00 +0100

    We know only encode QR (prevent leaking 1 bit of information)

--- src/main/java/fr/polytech/elgamalgreaux/ElGamal.java
+++ src/main/java/fr/polytech/elgamalgreaux/ElGamal.java
@@ -4,55 +4,49 @@ import java.util.List;
 
 public class ElGamal {
 
-    public static final int MESSAGE = 2;
-    public static final int SALT = 3;
+    public static final int MESSAGE = 1;
+
     public static void main(String[] args) {
-        //unsecure();
-        secure();
+        elGamal(MESSAGE);
+
+        /*
+        for(int i = -2; i < 47; i++) {
+            elGamal(i);
+        }
+        */
+
     }
 
-    private static void secure() {
+    public static void elGamal(int message) {
         System.out.println("ElGamal cryptography");
 
         Group group = new Group();
         Person alice = new Person(group);
         Person bob = new Person(group);
 
-        System.out.println("Message: " + ((MESSAGE)%group.getPrime()) + " residue quadratic? " + group.isQuadraticResidue(MESSAGE));
-        System.out.println("Message salted: " + ((MESSAGE+SALT)%group.getPrime()) + " residue quadratic? " + group.isQuadraticResidue(MESSAGE));
+        message %= group.getPrime();
 
-        List<Long> encrypted = bob.encrypt((MESSAGE+SALT), alice);
+        if(!group.isQuadraticResidue(message)) {
+            System.out.println(message + " is not a quadratic residue, exiting...");
+            return;
+        }
 
+
+        System.out.println("Message: " + message);
+
+        // Ciphering
+        List<Long> encrypted = bob.encrypt(message, alice);
+
+        // Verifying that the cipher is QR
         long c1 = encrypted.get(0);
         long c2 = encrypted.get(1);
         System.out.println("Encrypted: (" + c1 + ", " + c2 + ")");
         System.out.println(c2 + " residue quadratic? " + group.isQuadraticResidue((int) c2));
 
-        long m = alice.decrypt(encrypted);
-        System.out.println("Decrypted salted: " + m);
-        m -= SALT;
-        m %= group.getPrime();
-        System.out.println("Decrypted: " + m);
-    }
-
-    public static void unsecure() {
-
-        System.out.println("ElGamal cryptography");
-
-        Group group = new Group();
-        Person alice = new Person(group);
-        Person bob = new Person(group);
-
-        System.out.println("Message: " + (MESSAGE%group.getPrime()) + " residue quadratic? " + group.isQuadraticResidue(MESSAGE));
-
-        List<Long> encrypted = bob.encrypt(MESSAGE, alice);
-
-        long c1 = encrypted.get(0);
-        long c2 = encrypted.get(1);
-        System.out.println("Encrypted: (" + c1 + ", " + c2 + ")");
-        System.out.println(c2 + " residue quadratic? " + group.isQuadraticResidue((int) c2));
-
+        // Decrypting
         long m = alice.decrypt(encrypted);
         System.out.println("Decrypted: " + m);
     }
+
+
 }
